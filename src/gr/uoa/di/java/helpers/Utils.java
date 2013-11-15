dfea840868cc99030e92694912f13973fa2faac1
MrD <the.ubik@gmail.com>
2013-11-15 04:36:00 +0200

    Utils final + Zip

--- src/gr/uoa/di/java/helpers/Utils.java
+++ src/gr/uoa/di/java/helpers/Utils.java
@@ -2,10 +2,13 @@ package gr.uoa.di.java.helpers;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Utils {
+
+	private Utils() {}
 
 	// Standard charsets' NAMES
 	// see http://stackoverflow.com/questions/1684040/ for a discussion
@@ -31,48 +34,108 @@ public final class Utils {
 	 */
 	public static final String UTF16 = "UTF-16";
 
-	private Utils() {}
-
+	/**
+	 * Returns the String that the given List of Bytes represents in the given
+	 * charset. If an empty list is given the empty string is returned
+	 * immediately. Otherwise the Bytes are decoded using the charset specified
+	 * by its name. Note that if the Bytes given are not valid in the given
+	 * charset the behavior of this method is unspecified (due to the
+	 * unspecified behavior of the {@link String#String(byte[], String)}
+	 * constructor).
+	 *
+	 * @param lb
+	 *            a list of Bytes
+	 * @param charsetName
+	 *            the charset name those bytes will be decoded with
+	 * @return a sting constructed from the bytes in lb
+	 * @throws UnsupportedEncodingException
+	 *             if the charset given is unsupported (except if an empty list
+	 *             is given where the charset is not used)
+	 * @throws NullPointerException
+	 *             if lb is null
+	 */
 	public static String listToString(List<Byte> lb, String charsetName)
-			throws UnsupportedEncodingException, NumberFormatException {
+			throws UnsupportedEncodingException {
 		// see http://stackoverflow.com/questions/1096868/
-		// TODO better tests
 		if (lb == null)
 			throw new NullPointerException("List<Byte> can't be null");
-		byte[] array = new byte[lb.size()];
+		if (lb.isEmpty()) return "";
+		final byte[] array = new byte[lb.size()];
 		{
 			int i = 0;
 			for (byte current : lb) {
-				array[i] = current;
-				++i;
+				array[i++] = current;
 			}
 		}
-		return new String(array, charsetName);
-	}
-
-	public static long listToLong(List<Byte> lb)
-			throws UnsupportedEncodingException, NumberFormatException {
-		// TODO better tests
-		if (lb == null)
-			throw new NullPointerException("List<Byte> can't be null");
-		return new BigDecimal(listToString(lb, ASCII)).longValue();
+		return new String(array, charsetName); // The behavior of this
+		// constructor when the given bytes are not valid in the given charset
+		// is unspecified -- !!!!
 	}
 
 	/**
-	 * If list is empty 0 is returned. If List is null is thrown
+	 * Returns a long whose ASCII representation are the bytes in the given
+	 * list. If lb is null NullPointerException is thrown. If lb is empty
+	 * NumberFormatException is thrown. Otherwise lb must contain the ASCII
+	 * representation of the digits of a long number. If other characters are
+	 * contained a {@link NumberFormatException} is thrown. If characters
+	 * outside the ASCII range are contained the
+	 * {@link String#String(byte[], String)} constructor used internally will
+	 * result in unspecified behavior which in turn will lead to a
+	 * NumberFormatException (hopefully). Careful - the l/L long suffix must not
+	 * be included in the bytes - will result in NumberFormatException
 	 *
 	 * @param lb
+	 *            the list of bytes that represent the characters representing a
+	 *            double
 	 * @return
-	 * @throws UnsupportedEncodingException
+	 * @throws NumberFormatException
+	 *             if the bytes in lb are converted to a string that can't be
+	 *             converted to a double
 	 * @throws NullPointerException
-	 *             if ld is null
+	 *             if lb is null
 	 */
-	public static double listToDouble(List<Byte> lb)
-			throws UnsupportedEncodingException, NumberFormatException {
-		// TODO better tests
+	public static long listToLong(List<Byte> lb) throws NumberFormatException {
 		if (lb == null)
 			throw new NullPointerException("List<Byte> can't be null");
-		return new BigDecimal(listToString(lb, ASCII)).doubleValue();
+		try {
+			return new BigInteger(listToString(lb, ASCII)).longValue();
+		} catch (UnsupportedEncodingException e) {
+			throw new AssertionError(e); // "ASCII unsupported"
+		}
+	}
+
+	/**
+	 * Returns a double whose ASCII representation are the bytes in the given
+	 * list. If lb is null NullPointerException is thrown. If lb is empty
+	 * NumberFormatException is thrown. Otherwise lb must contain the ASCII
+	 * representation of the digits of a double number and possibly an exponent
+	 * or a decimal part etc. If other characters are contained a
+	 * {@link NumberFormatException} is thrown. If characters outside the ASCII
+	 * range are contained the {@link String#String(byte[], String)} constructor
+	 * used internally will result in unspecified behavior which in turn will
+	 * lead to a NumberFormatException (hopefully). Careful - the d/D double
+	 * suffix must not be included in the bytes - will result in
+	 * NumberFormatException
+	 *
+	 * @param lb
+	 *            the list of bytes that represent the characters representing a
+	 *            double
+	 * @return
+	 * @throws NumberFormatException
+	 *             if the bytes in lb are converted to a string that can't be
+	 *             converted to a double
+	 * @throws NullPointerException
+	 *             if lb is null
+	 */
+	public static double listToDouble(List<Byte> lb)
+			throws NumberFormatException {
+		if (lb == null)
+			throw new NullPointerException("List<Byte> can't be null");
+		try {
+			return new BigDecimal(listToString(lb, ASCII)).doubleValue();
+		} catch (UnsupportedEncodingException e) {
+			throw new AssertionError(e); // "ASCII unsupported"
+		}
 	}
 
 	public static List<Byte> listFromArray(byte[] ba) {
--- test/gr/uoa/di/java/helpers/test/UtilsTest.java
+++ test/gr/uoa/di/java/helpers/test/UtilsTest.java
@@ -4,14 +4,17 @@ import static gr.uoa.di.java.helpers.Utils.listFromArray;
 import gr.uoa.di.java.helpers.Utils;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
-
-import junit.framework.Assert;
+import java.util.List;
 
 import org.apache.http.util.EncodingUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UtilsTest {
+
+	private static final String UNKNOWN_ENCODING = "UnknownEncoding";
 
 	@Test
 	public void test_listToString() throws UnsupportedEncodingException {
@@ -21,26 +24,144 @@ public class UtilsTest {
 	}
 
 	@Test
-	public void test_listToLong() throws NumberFormatException,
-			UnsupportedEncodingException {
-		Assert.assertEquals(178899898, Utils
-				.listToLong(listFromArray((EncodingUtils
-						.getAsciiBytes("178899898")))));
+	public void test_listToString_Empty() throws UnsupportedEncodingException {
+		final Byte[] ba = new Byte[0];
+		final List<Byte> asList = Arrays.asList(ba);
+		Assert.assertTrue(asList.isEmpty());
+		Assert.assertEquals("", Utils.listToString(asList, UNKNOWN_ENCODING));
+	}
+
+	@Test(expected = UnsupportedEncodingException.class)
+	public void test_listToString_UnsupportedEncoding()
+			throws UnsupportedEncodingException {
+		System.out.println(Utils.listToString(
+			Arrays.asList((byte) 'h', (byte) 'e'), UNKNOWN_ENCODING));
+	}
+
+	/** BEHAVES AS NOTHING IS WRONG - need Java7 to make it throw */
+	@Test
+	public void test_listToString_UnsupportedCharacters()
+			throws UnsupportedEncodingException {
+		final Byte[] ba = new Byte[256];
+		{
+			byte b = Byte.MIN_VALUE;
+			for (int i = 0; i < 256; ++i) {
+				System.out.println(b);
+				ba[i] = b++;
+			}
+		}
+		System.out.println(Utils.listToString(Arrays.asList(ba), Utils.ASCII));
 	}
 
 	@Test
-	public void test_listToDouble() throws NumberFormatException,
-			UnsupportedEncodingException {
-		Assert.assertEquals(1788.9898, Utils
-				.listToDouble(listFromArray(EncodingUtils
-						.getAsciiBytes("1788.9898"))));
+	public void test_listToLong() throws NumberFormatException {
+		Assert.assertEquals(178899898,
+			Utils.listToLong(listFromArray((EncodingUtils
+				.getAsciiBytes("178899898")))));
 	}
-	// crap - if I pass integers in I get ClassCastEx
-	// private <T> List<Byte> castToByte(T... args) {
-	// final List<Byte> li = new ArrayList<Byte>();
-	// for (T t : args) {
-	// li.add((Byte) t);
-	// }
-	// return li;
-	// }
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToLong_SUFFIX1() throws NumberFormatException {
+		Assert.assertEquals(178899898, Utils
+			.listToLong(listFromArray((EncodingUtils
+				.getAsciiBytes("178899898l")))));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToLong_SUFFIX2() throws NumberFormatException {
+		Assert.assertEquals(178899898, Utils
+			.listToLong(listFromArray((EncodingUtils
+				.getAsciiBytes("178899898L")))));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listEmptyToLong() throws NumberFormatException {
+		Assert.assertEquals(0, Utils.listToLong(new ArrayList<Byte>()));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToLong_NumberFormatEx() throws NumberFormatException {
+		Utils.listToLong(listFromArray((EncodingUtils
+			.getAsciiBytes("1788@99898"))));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToLong_NumberFormatEx2() throws NumberFormatException {
+		Utils.listToLong(listFromArray((EncodingUtils
+			.getAsciiBytes("1788.99898"))));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToLong_NumberFormatEx3() throws NumberFormatException {
+		Utils.listToLong(listFromArray((EncodingUtils
+			.getAsciiBytes("1788,99898"))));
+	}
+
+	/* 1e1 2. .3 0.0 3.14 1e-9d 1e137 ---- More to try (from the JLS) */
+	@Test
+	public void test_listToDouble() throws NumberFormatException {
+		Assert.assertEquals(1788.9898, Utils
+			.listToDouble(listFromArray(EncodingUtils
+				.getAsciiBytes("1788.9898"))), Double.MIN_VALUE);
+	}
+
+	@Test
+	public void test_listToDouble2() throws NumberFormatException {
+		Assert.assertEquals(1788e98,
+			Utils.listToDouble(listFromArray(EncodingUtils
+				.getAsciiBytes("1788e98"))), Double.MIN_VALUE);
+	}
+
+	@Test
+	public void test_listToDouble3() throws NumberFormatException {
+		Assert.assertEquals(1788E98,
+			Utils.listToDouble(listFromArray(EncodingUtils
+				.getAsciiBytes("1788E98"))), Double.MIN_VALUE);
+	}
+
+	@Test
+	public void test_listToDouble4() throws NumberFormatException {
+		Assert.assertEquals(73.2, Utils
+			.listToDouble(listFromArray(EncodingUtils.getAsciiBytes("+73.2"))),
+			Double.MIN_VALUE);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToDouble_SUFFIX1() throws NumberFormatException {
+		Assert.assertEquals(1, Utils.listToDouble(listFromArray(EncodingUtils
+			.getAsciiBytes("1d"))), Double.MIN_VALUE);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToDouble_SUFFIX2() throws NumberFormatException {
+		Assert.assertEquals(-1, Utils.listToDouble(listFromArray(EncodingUtils
+			.getAsciiBytes("-1D"))), Double.MIN_VALUE);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listEmptyToDouble() throws NumberFormatException {
+		Assert.assertEquals(0, Utils.listToDouble(new ArrayList<Byte>()),
+			Double.MIN_VALUE);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToDouble_NumberFormatEx1()
+			throws NumberFormatException {
+		Utils.listToDouble(listFromArray((EncodingUtils
+			.getAsciiBytes("17885oio99898"))));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToDouble_NumberFormatEx2()
+			throws NumberFormatException {
+		Utils.listToDouble(listFromArray((EncodingUtils
+			.getAsciiBytes("17884*99898"))));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_listToDouble_NumberFormatEx3()
+			throws NumberFormatException {
+		Utils.listToDouble(listFromArray((EncodingUtils
+			.getAsciiBytes("1788()99898"))));
+	}
 }
